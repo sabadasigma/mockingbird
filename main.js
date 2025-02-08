75e9596bbcb21e1caaa820ff722b8c4c20695b38
sabadasigma <tsita.saba@gmail.com>
2025-02-08 15:05:22 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const user1 = "giorgi";
+const age1 = 20;
+
+
+const user2 = "mariam";
+const age2 = 18;
+
+const color1 = "black";
+
+const color2 = "purple";
+
+const color3 = "red";
+
+const num1 = 17
+
+const num2 = -7
+
+const num3 = 15 * 3;
+const num4 = 15 + 3;
+const num5 = 15 - 3;
+const num6 = 15 / 3;
+
+const num7 = 123 * 654 
+
+console.log(num7)
+
+
+console.log(num3)
+
+console.log(num5)
